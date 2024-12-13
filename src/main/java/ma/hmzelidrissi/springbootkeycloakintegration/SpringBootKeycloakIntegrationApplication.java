fe131e816aa7d46f6f0cf17beb9da0e0fbef9309
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-12-13 12:39:48 +0100

    Configure spring security

---
+++ src/main/java/ma/hmzelidrissi/springbootkeycloakintegration/SpringBootKeycloakIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ma.hmzelidrissi.springbootkeycloakintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloakIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKeycloakIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/hmzelidrissi/springbootkeycloakintegration/SpringBootKeycloakIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.hmzelidrissi.springbootkeycloakintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
