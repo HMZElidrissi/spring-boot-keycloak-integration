069307de997fde584f903025dc92a7ecb60743de
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-12-14 14:30:48 +0100

    clean up

--- src/main/java/ma/hmzelidrissi/springbootkeycloakintegration/SecurityConfig.java
+++ src/main/java/ma/hmzelidrissi/springbootkeycloakintegration/SecurityConfig.java
@@ -1,6 +1,5 @@
 package ma.hmzelidrissi.springbootkeycloakintegration;
 
-import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,8 +8,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/ma/hmzelidrissi/springbootkeycloakintegration/SpringBootKeycloakIntegrationApplication.java
+++ src/main/java/ma/hmzelidrissi/springbootkeycloakintegration/SpringBootKeycloakIntegrationApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootKeycloakIntegrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootKeycloakIntegrationApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootKeycloakIntegrationApplication.class, args);
+  }
 }
